51448d050e0aa2e20fecc1103b971fceea4b61b4
Shaji Khan <quotablecode@gmail.com>
2025-02-03 20:47:11 +0530

    seemingly it works but not yet shall I rejoice success is a cruel mistress

--- app/src/main/cpp/Plugin.cpp
+++ app/src/main/cpp/Plugin.cpp
@@ -459,7 +459,7 @@ void Plugin::setAtomPortValue (int control, std::string text) {
 //    ampAtom->send_filename_to_plugin(ampMap, text.c_str(), reinterpret_cast<uint8_t *>(filePort), 8192 + sizeof (LV2_Atom));
 //    ampAtom->son_of_a(filePort, text.c_str());
     ampAtom->write_control(filePort, filePortSize, text.c_str());
-    ampAtom->write_control(notifyPort, filePortSize, text.c_str());
+//    ampAtom->write_control(notifyPort, filePortSize, text.c_str());
 //    ampAtom->setControl(filePort, const_cast<char *>(text.c_str()));
     OUT
 }
